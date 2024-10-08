9f6fc735adc1fd66e6be399a864874f7e27be415
Kacper Zalewski <kzalewski433@gmail.com>
2024-10-08 18:14:24 -0500

    w

--- Week5Algorithms/src/Test.java
+++ Week5Algorithms/src/Test.java
@@ -1,16 +1,15 @@
 public class Test {
     public static void main(String[] args) {
-        Stack<Integer> S = new ArrayStack<>(); // contents: ()
+        Stack<Integer> S = new ArrayStack<>();
         for (int i = 0; i < 2; i++)
             S.pop();
-            System.out.println(S.pop()); // contents: () outputs null
         for (int i = 0; i < 24; i++)
             S.push(3);
         for (int i = 0; i < 24; i++)
             S.top();
         for (int i = 0; i < 6; i++)
             S.pop();
-        System.out.println("Final stack size is: " + S.size()); // contents: (5, 3) outputs 2
+        System.out.println("Final stack size is: " + S.size()); 
 
     }
 }
